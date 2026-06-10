d56ef705d3cc525cd702fd46482c0e98c140d736
aayushhh12 <aayukothari888@gmail.com>
2026-06-11 01:41:21 +0530

    Added a comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import UserContext from "./context/UserContext";
+// This is
 
 const Footer = lazy(() => import("./components/Footer"));
 const Search = lazy(() => import("./pages/Search"));
